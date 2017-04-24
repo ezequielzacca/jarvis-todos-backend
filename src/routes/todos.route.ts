75cc75dab3a1e603853cad312f67e12a069976b4
Ezequiel Zacca <ezequielzaccar@gmail.com>
2017-04-24 13:20:48 -0300

    speech provided

--- src/routes/todos.route.ts
+++ src/routes/todos.route.ts
@@ -33,7 +33,7 @@ export class TodoRouter {
    */
   public apiaiHook(req: Request, res: Response, next: NextFunction) {
     console.log("Hook Request");
-    let speech:string = 'empty speech';
+    let speech: string = 'empty speech';
 
     if (req.body) {
       var requestBody = req.body;
@@ -55,13 +55,13 @@ export class TodoRouter {
               throw err;
             }
             let possibleAnswers = [
-                `${requestBody.result.parameters.todo} added to your list, anything else?`,
-                `I've just added ${requestBody.result.parameters.todo} to your list, can i help you with something else?`,
-                `Your wishes are orders, ${requestBody.result.parameters.todo} added, any other Todo?`,
-                `${requestBody.result.parameters.todo} shipped right onto the list, can i do anything else for you?`,
-                `Done, ${requestBody.result.parameters.todo} was added, can i do any more things for you?`,
-                `Im happy to annouce you that ${requestBody.result.parameters.todo} was added, if you need anything else...`
-               ]
+              `${requestBody.result.parameters.todo} added to your list, anything else?`,
+              `I've just added ${requestBody.result.parameters.todo} to your list, can i help you with something else?`,
+              `Your wishes are orders, ${requestBody.result.parameters.todo} added, any other Todo?`,
+              `${requestBody.result.parameters.todo} shipped right onto the list, can i do anything else for you?`,
+              `Done, ${requestBody.result.parameters.todo} was added, can i do any more things for you?`,
+              `Im happy to annouce you that ${requestBody.result.parameters.todo} was added, if you need anything else...`
+            ]
             let random = Math.floor(Math.random() * possibleAnswers.length);
             console.log("random value: ", random);
             let speech = possibleAnswers[random];
@@ -71,17 +71,17 @@ export class TodoRouter {
               source: 'apiai-webhook-sample'
             });
           });
-        }else if (requestBody.result.action == "delete_todo") {
+        } else if (requestBody.result.action == "delete_todo") {
           //speech += 'action: ' + requestBody.result.action;
           console.log("Action is ", requestBody.result.action);
           console.log("Todo to delete is ", requestBody.result.parameters.todo);
-          database.getDB().collection('todos').remove({$text:{$search:requestBody.result.parameters.todo}}, (err, removed) => {
+          database.getDB().collection('todos').remove({ $text: { $search: requestBody.result.parameters.todo } }, (err, removed) => {
             if (err) {
               throw err;
             }
             console.log(removed.result.n);
             let cantidad = removed.result.n;
-            if(cantidad>0){
+            if (cantidad > 0) {
               let possibleAnswers = [
                 `${cantidad} todos matched and removed from your list, anything else?`,
                 `I've just removed ${cantidad} todos that matched, can i help you with something else?`,
@@ -89,25 +89,31 @@ export class TodoRouter {
                 `${cantidad} todos finished their existence, can i do anything else for you?`,
                 `Done, ${cantidad} todos were deleted, can i do any more things for you?`,
                 `Im happy to annouce you that ${cantidad} todos were kicked from your list, if you need anything else...`
-               ]
-            let random = Math.floor(Math.random() * possibleAnswers.length);
-            console.log("random value: ", random);
-            speech = possibleAnswers[random];
-            }else{
+              ]
+              let random = Math.floor(Math.random() * possibleAnswers.length);
+              console.log("random value: ", random);
+              let speech = possibleAnswers[random];
+              return res.json({
+                speech: speech,
+                displayText: speech,
+                source: 'apiai-webhook-sample'
+              });
+            } else {
               let possibleAnswers = [
                 `Sorry, but i could not find any todo matching that criteria.`,
                 `I've just removed... Oops, 0 todos match those words... maybe try a different term`,
                 `Your wishes are orders, and i wish i could find any todo, but with those words there ar 0`,
-               ]
-               let random = Math.floor(Math.random() * possibleAnswers.length);
-               speech = possibleAnswers[random];
+              ]
+              let random = Math.floor(Math.random() * possibleAnswers.length);
+              let speech = possibleAnswers[random];
+              return res.json({
+                speech: speech,
+                displayText: speech,
+                source: 'apiai-webhook-sample'
+              });
             }
-            
-            return res.json({
-              speech: speech,
-              displayText: speech,
-              source: 'apiai-webhook-sample'
-            });
+
+
           });
         } else if (requestBody.result.action == "input.unknown") {
           //speech += 'action: ' + requestBody.result.action;
@@ -118,10 +124,10 @@ export class TodoRouter {
           });
           let document = languageClient.document(input);
           document.detectSentiment(function (err, sentiment) {
-            console.log("err: ",err);
-            console.log("sentiment: ",sentiment);
-            console.log("sentiment is positive: ",sentiment.score > 0.3);
-            console.log("sentiment is negative: ",sentiment.score <= -0.3);
+            console.log("err: ", err);
+            console.log("sentiment: ", sentiment);
+            console.log("sentiment is positive: ", sentiment.score > 0.3);
+            console.log("sentiment is negative: ", sentiment.score <= -0.3);
             if (sentiment) {
               if (sentiment.score <= -0.3) {
                 return res.json({
@@ -129,7 +135,7 @@ export class TodoRouter {
                   displayText: "Whoaaa take it easy man, im just a poor bot",
                   source: 'apiai-webhook-sample'
                 });
-              }else if (sentiment.score > 0.3) {
+              } else if (sentiment.score > 0.3) {
                 return res.json({
                   speech: "Its always great to talk with nice people",
                   displayText: "Its always great to talk with nice people",
